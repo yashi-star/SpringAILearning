1c39aa574db74e6066d397c2e064790c1faa97dd
yashi-star <pantyashi54@gmail.com>
2026-01-28 09:03:37 +0530

    new project

---
+++ prompt/prompt/src/test/java/com/springai/prompting/prompt/PromptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai.prompting.prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
